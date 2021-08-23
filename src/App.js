2cfaa980cfc395da966b6b04c579e8c9116b29b2
김하진 <knj9417@gmail.com>
2021-08-24 01:12:03 +0900

    today

--- src/App.js
+++ src/App.js
@@ -52,7 +52,6 @@ function Food({ name, picture, rating }) {
 Food.propTypes = {
   name: PropTypes.string.isRequired,
   picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
 };
 
 function App() {
