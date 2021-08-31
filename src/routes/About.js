d81fb55c9b56b32986faaa400f40b7c1e85f2951
김하진 <knj9417@gmail.com>
2021-08-31 15:48:58 +0900

    Sharing Props Between Routes

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,10 +5,10 @@ function About() {
   return (
     <div className="about__container">
       <span>
-        “Freedom is the freedom to say that two plus two make four. If that is
+        “Insaeng is the freedom to say that don't forget own patient. If that is
         granted, all else follows.”
       </span>
-      <span>− George Orwell, 1984</span>
+      <span>− Hajin Kim, 2021</span>
     </div>
   );
 }
