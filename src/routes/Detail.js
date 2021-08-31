9843f8fd269a614ea07687238568c7ca45765f16
김하진 <knj9417@gmail.com>
2021-08-31 16:56:49 +0900

    Redirecting

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,21 @@
+import React from "react";
 import Reat from "react";
 
-function Detail({ location }) {
-  console.log(location);
-  return <span>Hello!</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
